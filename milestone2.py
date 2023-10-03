323740acefb7642d25898bea64480d354b7c4fde
Eleftherios <eleytherios8@gmail.com>
2023-10-03 14:07:00 +0100

    hangman prject with milestone 3 tasks

--- milestone2.py
+++ milestone2.py
@@ -3,8 +3,29 @@ import random
 word_list = ["banana", "apple", "grapes", "cherries", "orange"]
 word = random.choice(word_list)
 print(word)
-guess = input("Enter a single letter  ")
-if (len(guess) == 1) and (guess.isalpha()):
-    print("Good guess")
-else:
-    print("Ooops That is not a valid input")
+
+
+
+def ask_for_input ():
+    guess = input("Enter a single letter  ")
+    check_guess(guess)
+    while True:
+        if (len(guess) == 1) and (guess.isalpha() == True):
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.  ")
+            guess = input("Enter a single letter  ")
+            
+
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print("Good guess!  {} is in the word".format(guess))
+        return guess
+    else:
+        print("Sorry, {}  is not in the word, Try again!".format(guess))
+        return guess
+
+
+ask_for_input()
