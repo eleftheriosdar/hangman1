867f4dc43f24e95961ac99013c443578f095b20b
Eleftherios <eleytherios8@gmail.com>
2023-10-03 14:09:39 +0100

    milestone 3 tasks

--- milestone_3.py
+++
@@ -1,6 +0,0 @@
-while True:
-    guess = input("enter a letter  ")
-    if guess .isalpha() == True:
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.  ")
